d21bce84cf35d1166b628cb373c1a5dc317b8962
ColyYang <15313000686@163.com>
2020-04-16 15:32:41 +0800

    finish

--- assignment/Clock/app/src/main/java/com/bytedance/clockapplication/widget/Clock.java
+++ assignment/Clock/app/src/main/java/com/bytedance/clockapplication/widget/Clock.java
@@ -106,7 +106,7 @@ public class Clock extends View {
         drawNeedles(canvas);
 
         // todo 每一秒刷新一次，让指针动起来
-
+        postInvalidateDelayed(1000);
     }
 
     private void drawDegrees(Canvas canvas) {
@@ -167,9 +167,14 @@ public class Clock extends View {
         drawPointer(canvas, 2, nowSeconds);
         // 画分针
         // todo 画分针
+//        int new_part=nowSeconds/12;
+//        drawPointer(canvas, 1, 5*nowMinutes + new_part);
+        drawPointer(canvas, 1, nowMinutes);
+
         // 画时针
         int part = nowMinutes / 12;
         drawPointer(canvas, 0, 5 * nowHours + part);
+
 
 
     }
@@ -189,7 +194,9 @@ public class Clock extends View {
                 break;
             case 1:
                 // todo 画分针，设置分针的颜色
-
+                degree = value * UNIT_DEGREE;
+                mNeedlePaint.setColor(Color.BLUE);
+                pointerHeadXY = getPointerHeadXY(MINUTE_POINTER_LENGTH, degree);
                 break;
             case 2:
                 degree = value * UNIT_DEGREE;
